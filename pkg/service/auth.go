9d93a4597a4bac74d80d4657ba04c9db0f77cea5
CheStix <che.eldar@gmail.com>
2023-09-11 17:07:25 +0300

    authenticate, jwt token

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,9 +5,20 @@ import (
 	"fmt"
 	"github.com/CheStix/todo-app"
 	"github.com/CheStix/todo-app/pkg/repository"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
 )
 
-const salt = "sdfslkfslkfslfkhsfh"
+const (
+	salt       = "sdfslkfslkfslfkhsfh"
+	signingKey = "dhsjkhgsuyr7895jhhHGkss84hjGFkoe8"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.RegisteredClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -20,6 +31,23 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(tokenTTL)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
